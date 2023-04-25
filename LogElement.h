a904730f8db354e1ac7cbf49238abf58c1202abb
MiYuri <alli13let@gmail.com>
2023-04-25 21:43:22 +0400

    Added functions to the 'public' section

--- LogElement.h
+++ LogElement.h
@@ -5,3 +5,13 @@ private:
 	bool In2;
 protected:
 	bool Res;
+public:
+	void setIn1(bool newIn1);
+	void setIn2(bool newIn2);
+	bool getResOr();
+	bool getResNot();
+	bool getResAnd();
+	bool getResNand();
+	bool getResNor;
+ 	bool getResXor;
+}; 
